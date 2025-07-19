7db58536a331dfb3231fc83b1943542ddd6c9fcf
Mirela2023 <mirelacertsign@gmail.com>
2025-07-19 21:41:59 +0300

    Cel mai mare numar din trei - varianta scurta

---
+++ 05_Pachet_suplimentar_1/06a_cel_mai_mare_numar.py
@@ -0,0 +1,8 @@
+# 6. Află cel mai mare din trei numere
+# Exemplu: a = 4, b = 9, c = 1 → Output: 9
+
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
+print(f"Cel mai mare numar dintre {a}, {b}, {c} este {max(a, b, c)}.")
